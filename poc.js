ea555089226aaff46ff70a29569b7bd7dcb4b210
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2021-04-27 12:44:18 +0100

    add a second method - claim tokens, add simple strategies, refactoring

--- poc.js
+++ poc.js
@@ -5,11 +5,14 @@ const web3 = new Web3("https://bsc-dataseed.binance.org/")
 const { eth, utils } = web3
 const { Contract } = eth
 const { toWei } = utils
-// const getTransaction = promisify(eth.getTransaction)
 
-// address: 0x5870a2af7ede1B1995426f640B407F888b34b0A7
+// BNB bunny proxy address
+const contractAddress = "0x52cfa188a1468a521a98eaa798e715fbb9eb38a3"
 
-// lib
+// CAKE bunny proxy address
+// const contractAddress = "0xedfcb78e73f7ba6ad2d829bf5d462a0924da28ed"
+
+// lib - TODO: extract
 
 const signTransaction = (txAttrs, privateKey) => {
   return new Promise((resolve, reject) => {
@@ -40,55 +43,50 @@ if (!pvtKey) {
 const account = eth.accounts.privateKeyToAccount(pvtKey)
 const { address } = account
 
-;(async () => {
+const pools = ["bnb", "cake"]
+const pool = "bnb"
+const abiPath = `abi/${pool}`
+const ABI = require(abiPath)
 
-  const pools = ["bnb", "cake"]
-  const pool = "bnb"
-  const abiPath = `abi/${pool}`
-  const ABI = require(abiPath)
+const claimAndWithdrawTokens = () => {
+  // TODO:
+}
 
-  // const block = await eth.getBlockNumber()
-  // console.log("block:", block)
-
-  // BNB bunny proxy address
-  const contractAddress = "0x52cfa188a1468a521a98eaa798e715fbb9eb38a3"
-
-  // CAKE bunny proxy address
-  // const contractAddress = "0xedfcb78e73f7ba6ad2d829bf5d462a0924da28ed"
-
-  // const txToCopy = "0xbdd5a03cecf3a85569dd21071024ab161d6f193a225ab9221e050966a47a7390"
-  //
-  // const tx = await eth.getTransaction(txToCopy)
-  // console.log("tx:", tx)
-
-  // const contractAddress = "0xedfcb78e73f7ba6ad2d829bf5d462a0924da28ed"
-  //
-  // const functionName = "getReward()"
-  // MethodID: 0x3d18b912
-
-  // ABI BNB
-  const ABI = require("./abi/bnb.js")
-
-
-  // ABI CAKE
-  // -----
-  // const ABI = require("./abi/cake.js")
-  // const code = await eth.getCode(contractAddress)
-  // console.log("code:", code)
-
+const claimTokens = () => {
   let swap = new Contract(ABI, contractAddress)
   swap = swap.methods
 
-  // const toAddress = address   // send to self
-  // const value = 1000000000000 // min eth (bnb) transfer amount (in wei)
+  const getRewardData = swap.getReward().encodeABI()
+
+  // TODO: estimategas
+  const txAttrs = {
+    gasPrice: "5000000000", // 5 gwei - min gasPrice in BSC
+    // gas:      "21000", // normal tx
+    gas:      "552000", // normal tx
+    to:       contractAddress,
+    data:     getRewardData,
+  }
+
+  const rawTx = await signTransaction(txAttrs, pvtKey)
+  // console.log("signedTx:", signedTx)
+
+  const txHashPromise = eth.sendSignedTransaction(rawTx)
+  const txHash = resolveTxHash(txHashPromise)
+  console.log("txHash:", txHash)
+}
+
+const depositTokens = () => {
+  let swap = new Contract(ABI, contractAddress)
+  swap = swap.methods
 
   const depositBNBData = swap.depositBNB().encodeABI()
 
+  // sample deposit amount
   const depositAmountBNB = "0.001"
   const depositAmount = toWei(depositAmountBNB, "ether")
 
-
   const data = depositBNBData
+  // TODO: estimategas
   const txAttrs = {
     gasPrice: "5000000000", // 5 gwei - min gasPrice in BSC
     // gas:      "21000", // normal tx
@@ -98,26 +96,46 @@ const { address } = account
     data:     data,
   }
 
-  // dev account: 0x5870a2af7ede1B1995426f640B407F888b34b0A7
-
-  // bunny:
-  // deposit BNB call
-  // ~0.003 - 0.002856 gas fee in bnb (eth)
-  // 551119 gas
-
-  // contract call n1
-  // depositBNB()
-
-
   const rawTx = await signTransaction(txAttrs, pvtKey)
   // console.log("signedTx:", signedTx)
-
 
   const txHashPromise = eth.sendSignedTransaction(rawTx)
   const txHash = resolveTxHash(txHashPromise)
   console.log("txHash:", txHash)
+}
 
 
-  // const res = await swap.getReward().call()
-  // console.log("res:", res)
+const TOKEN = "BNB"
+const deinvestTokenPriceTreshold = 510
+
+const deinvest = async () => {
+  await claimAndWithdrawTokens()
+}
+
+const reinvest = async () => {
+  await claimTokens()
+  await zapToken()
+  await depositTokens()
+}
+
+const reinvestLoop = async () => {
+  // if (oneWeekPassed) {
+  //   await reinvest()
+  // }
+}
+
+const deinvestLoop = async () => {
+  const price = await tokenPrice()
+  const balance = await tokenPoolBalance()
+
+  if (price < deinvestTokenPriceTreshold && balance > 0) {
+    await deinvest()
+  }
+}
+
+;(async () => {
+
+  const fiveMinutes = 1000*60*5
+  setInterval(deinvestLoop, fiveMinutes)
+
 })()
